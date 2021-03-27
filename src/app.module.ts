a6f81de5e6b4046859adc36236ca67ef71f6c7ed
Alem Getu <alem.t.getu@gmail.com>
2021-03-27 12:31:18 -0400

    configure database with typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ExampleModule } from './example_module/example.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
+//import * as ormconfig from '../ormconfig';
 
 @Module({
-  imports: [ExampleModule, TasksModule],
+  imports: [TasksModule, TypeOrmModule.forRoot()],
 })
 export class AppModule {}
